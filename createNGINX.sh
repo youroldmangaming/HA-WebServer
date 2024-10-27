73996ab95c9203939ab03076f06cc9d313506866
youroldmangaming <youroldmangaming@gmail.com>
2024-10-28 06:33:32 +1300

    Update createNGINX.sh

--- createNGINX.sh
+++ createNGINX.sh
@@ -1,1 +1,3 @@
-docker service create --name nginx-service --replicas 5 --publish 81:80 nginx
+docker service create --name nginx-service --replicas 5 --publish 81:80 \
+  --mount type=bind,source=$(pwd)/html,target=/usr/share/nginx/html \
+  nginx
